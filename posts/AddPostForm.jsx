232e7c7cf60952494a0245595243bc1ab13d0d61
Marian <hellomariandev@gmail.com>
2023-05-29 23:06:28 +0200

    adding the new post form

---
+++ posts/AddPostForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+
+  return (
+    <div>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        /> <br />
+        <label htmlFor="postContent">Content:</label>
+        <input
+          type="text"
+          id="postContent"
+          value={content}
+          onChange={onContentChanged}
+        /> <br />
+        <button type="button">Save Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddPostForm;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import AddPostForm from "../posts/AddPostForm";
 import PostsList from "../posts/PostsList";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <AddPostForm />
       <PostsList />
     </div>
   );
