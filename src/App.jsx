9df749c90cf51eb0f0c537816029ee2c5d6ed31e
Marian <hellomariandev@gmail.com>
2023-05-30 23:29:53 +0200

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import AddPostForm from "../posts/AddPostForm";
 import PostsList from "../posts/PostsList";
+import SinglePostPage from "../posts/SinglePostPage";
+
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <AddPostForm />
-      <PostsList />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddPostForm />
+              <PostsList />
+            </>
+          }
+        />
+
+        <Route path="/posts/:postId" component={<SinglePostPage />} />
+      </Routes>
+    </Router>
   );
 }
 
