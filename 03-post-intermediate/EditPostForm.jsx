490dfdfc0fb678ad270e829fa84cdb644e171799
Marian <hellomariandev@gmail.com>
2023-06-18 22:18:22 +0200

    edit post form

--- 03-post-intermediate/EditPostForm.jsx
+++ 03-post-intermediate/EditPostForm.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { postUpdated } from "./postsSlice";
 
 const EditPostForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const postId = params.id;
+
+  const post = useSelector((state) =>
+    state.posts.find((post) => post.id === postId)
+  );
+
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postUpdated({ id: postId, title, content }));
+      navigate(`/posts/${postId}`);
+    }
+  };
+
   return (
-    <div>
+    <div className="container">
       <h2>Edit Post Form</h2>
+      <form className="container">
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          placeholder="What's on your mind?"
+          value={title}
+          onChange={onTitleChanged}
+        />
+
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
+      </form>
     </div>
   );
 };
