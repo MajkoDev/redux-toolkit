5c494c718aee6d32b76aad22ff2deb8cdbdfc2c4
Marian <hellomariandev@gmail.com>
2023-06-18 17:28:49 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
 import Counter from "../01-counter/Counter";
 
 // import AddPostForm from "../02-post-simple/AddPostForm";
 // import PostsList from "../02-post-simple/PostsList";
 
-import AddPostForm from "../03-post-intermediate/AddPostForm";
+import RootLayout from "../03-post-intermediate/RootLayout";
 import PostsList from "../03-post-intermediate/PostsList";
 import SinglePostPage from "../03-post-intermediate/SinglePostPage";
-import Navbar from "../03-post-intermediate/Navbar";
-import { Route, Routes } from "react-router-dom";
+
+import AddPostForm from "../03-post-intermediate/AddPostForm";
+import EditPostForm from "../03-post-intermediate/EditPostForm";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<PostsList />} />
+      <Route path="posts/:id" element={<SinglePostPage />} />
+
+      <Route path="add" element={<AddPostForm />} />
+      <Route path="edit" element={<EditPostForm />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <PostsList /> 
-              <AddPostForm />
-            </>
-          }
-        />
-        
-      </Routes>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
