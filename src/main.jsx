9b3f96efabbfcfa74cae5cc207c89b241c96eded
Marian <hellomariandev@gmail.com>
2023-06-18 17:28:33 +0200

    newer react router

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "../03-post-intermediate/store";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
         <App />      
-      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
