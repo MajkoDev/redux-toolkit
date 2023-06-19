9be25021eb67aa9c302c5045a9fda892bcb51ff8
Marian <hellomariandev@gmail.com>
2023-06-19 23:04:51 +0200

    add post component

--- 03-post-intermediate/AddPostForm.jsx
+++ 03-post-intermediate/AddPostForm.jsx
@@ -1,58 +1,80 @@
-import React, { useState } from 'react'
-import { nanoid } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
-import { postAdded } from './postsSlice'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { postAdded } from "./postsSlice";
+import { useNavigate } from "react-router-dom";
 
 const AddPostForm = () => {
 
-    const dispatch = useDispatch()
+    const navigate = useNavigate();
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
 
-    function onTitleChange(e){
-        setTitle(e.target.value)
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users);
+
+  function onTitleChange(e) {
+    setTitle(e.target.value);
+  }
+  function onContentChange(e) {
+    setContent(e.target.value);
+  }
+  function onAuthorChange(e) {
+    setUserId(e.target.value);
+  }
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postAdded(title, content, userId));
+      setTitle("");
+      setContent("");
+      navigate(`/`);
+
     }
+  };
 
-    function onContentChange(e){
-        setContent(e.target.value)
-    }
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
-    const handleSubmit = () => {
-        // if we have title and content
-        if(title && content){
-            // 'dispatch' function calls 'postAdded' action
-            dispatch(postAdded({
-                // we take title and content from useState hooks 
-                // and generate new ID
-                id: nanoid(), title, content
-            }))
-            setTitle('')
-            setContent('')
-        }
-    }
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
-    <div className="container" style={{'marginTop': '40px'}}>
-        <h2>Add a New Post</h2>
-        <form className="container">
+    <div className="container" style={{ marginTop: "40px" }}>
+      <h2>Add a New Post</h2>
+      <form className="container">
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChange}
+        />
 
-            <label htmlFor="postTitle">Post Title:</label>
-            <input type="text" id='postTitle' name='postTitle'
-                value={title} 
-                onChange={onTitleChange} />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChange}>
+          {usersOptions}
+        </select>
 
-            <label htmlFor="postContent">Content:</label>
-            <textarea name="postContent" id="postContent"
-                value={content} 
-                onChange={onContentChange}>    
-            </textarea>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          name="postContent"
+          id="postContent"
+          value={content}
+          onChange={onContentChange}
+        ></textarea>
 
-            <button type='button'
-                onClick={handleSubmit}>Save Post</button>
-        </form>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddPostForm
+export default AddPostForm;
